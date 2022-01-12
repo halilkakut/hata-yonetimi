3816acd41dcd7fc1d401ea1bae03213991e9f5d1
Halil İbrahim Kakut <halil.kakut@hotmail.com>
2022-01-12 23:32:14 +0300

    Add

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try
+            // { 
+            //     Console.WriteLine("Bir Sayı giriniz:");
+            //     int sayi = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine("Sayı:"  +sayi);
+            // }
+            // catch (Exception ex) 
+            // {
+            //     Console.WriteLine("Hata: " +ex.Message.ToString());
+            // }
+            // finally {
+            //     Console.WriteLine("İşlem Tamamandı");
+            // }
+            
+            try
+            {
+                 //int a = int.Parse(null);
+                //  int a = int.Parse("test");
+                int a = int.Parse("-2000000000000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+               Console.WriteLine("Boş Değer Girdiniz.");
+               Console.WriteLine(ex);
+            }
+            catch(FormatException ex){
+                Console.WriteLine("Veri Tipi uygun değil");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex){
+                Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz.");
+                Console.WriteLine(ex);
+            }finally{
+                Console.WriteLine("İşlem Başarıyla tamamlandı.");
+            }
+        }
+    }
+}
